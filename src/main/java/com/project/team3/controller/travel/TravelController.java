74af895f9dc0fa245c5e25f9c82a8d2a707914a6
choi0229 <elcax29@gmail.com>
2024-09-20 11:53:40 +0900

    api암호화끝
    api암호화하고 gitignore에 env파일 추가

--- src/main/java/com/project/team3/controller/travel/TravelController.java
+++ src/main/java/com/project/team3/controller/travel/TravelController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.project.team3.dao.TravelDAO;
 import com.project.team3.vo.Travel;
 import com.project.team3.vo.User;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
@@ -20,11 +22,20 @@ public class TravelController {
 	TravelDAO dao;
 	
     @GetMapping("/travelMap.do")
-    public String showTravelPage(HttpServletRequest req, HttpSession session) {
+    public String showTravelPage(HttpServletRequest req, HttpSession session, Model model) {
     	List<Travel> travelList = dao.getTravelList();
     	User user = (User)session.getAttribute("userId");
     	req.setAttribute("user",user.userId);
     	req.setAttribute("travelList", travelList);
+    	Dotenv dotenv = Dotenv.configure()
+    		    .filename("kakaoApiKey.env") // 파일명 명시
+    		    .directory("C:/lx/spring/workspace/team03") // .env 파일이 위치한 디렉토리
+    		    .load();
+
+    		String kakaoApiKey = dotenv.get("KAKAO_API_KEY");
+         // 모델에 kakaoApiKey를 추가해서 Thymeleaf로 전달
+         model.addAttribute("kakaoApiKey", kakaoApiKey);
+
         return "/travel/travelMap"; 
     }
 
